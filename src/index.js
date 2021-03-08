e4d514d7aa59cb1f5cd30179b3ce0a4d4f41a780
XxeP <neg-nazarov@rambler.ru>
2021-03-08 09:10:14 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) {
+    return 0
+  } else {
+    let i = array[0]
+    array.forEach(x => {
+      if (x < i) {
+        i = x
+      }
+    });
+    return i;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+ if (!array || array.length == 0) {
+    return 0
+  } else {
+    let i = array[0]
+    array.forEach(x => {
+      if (x > i) {
+        i = x
+      }
+    });
+    return i;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if (!array || array.length == 0 ) {
+    return 0
+  } else {
+  let i = 0
+    array.forEach(x => {
+      i += x
+    });
+    return i / array.length;
+  }
 }
